9888bc11f4ae1eba6c08649a8fab91fd07077841
Wallysson <wlc.couto@gmail.com>
2022-05-04 00:24:17 -0300

    class 2

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -1,10 +1,15 @@
 import html2canvas from "html2canvas";
-import { Camera } from "phosphor-react";
+import { Camera, Trash } from "phosphor-react";
 import { useState } from "react";
 import { Loading } from "../Loading";
 
+interface ScreenshotButtonProps {
+  screenshot: string | null;
+  onScreenshotTook: (screenshot: string | null) => void;
+}
 
-export function ScreenshotButton() {
+
+export function ScreenshotButton({screenshot, onScreenshotTook}: ScreenshotButtonProps) {
   const [isTakingScreenshot, setIsTakingScreenshot] = useState(false)
 
   async function handleTakeScreenshot() {
@@ -12,9 +17,27 @@ export function ScreenshotButton() {
 
     const canvas = await html2canvas(document.querySelector('html')!)
     const base64image = canvas.toDataURL('image/png')
-    console.log(base64image)
+    
+    onScreenshotTook(base64image)
 
     setIsTakingScreenshot(false)
+    }
+
+    if (screenshot) {
+      return (
+        <button
+          type="button"
+          onClick={() => onScreenshotTook(null)}
+          className="p-1 w-10 h-10 rounded-md border-transparent flex justify-end items-end text-zinc-400 hover:text-zinc-100 transition-color"
+          style={{
+            backgroundImage: `url(${screenshot})`, 
+            backgroundPosition: 'right bottom',
+            backgroundSize: 100
+          }}
+        >
+          <Trash weight="fill"/> 
+        </button>
+      )
     }
 
   return (
