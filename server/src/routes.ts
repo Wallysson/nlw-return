7075081c16b4a359976c06701fefedcd203bf4f7
Wallysson <wlc.couto@gmail.com>
2022-05-09 21:53:31 -0300

    try and catch in routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,16 +8,18 @@ export const routes = express.Router();
 routes.post('/feedbacks', async (req, res) => {
   const {type, comment, screenshot} = req.body
 
-  const prismaFeedbacksRepository = new PrimaFeedbacksRepository()
-  const nodemailerMailAdapter = new NodemailerMailAdapter()
-
-  const submitFeedbackUseCase = new SubmitFeedbackCase(prismaFeedbacksRepository, nodemailerMailAdapter)
-
-  await submitFeedbackUseCase.execute({
-    type, comment, screenshot
-  })
-
-
-
-  return res.status(201).send()
+  try {
+    const prismaFeedbacksRepository = new PrimaFeedbacksRepository()
+    const nodemailerMailAdapter = new NodemailerMailAdapter()
+  
+    const submitFeedbackUseCase = new SubmitFeedbackCase(prismaFeedbacksRepository, nodemailerMailAdapter)
+    
+    await submitFeedbackUseCase.execute({
+      type, comment, screenshot
+    })
+    return res.status(201).send()
+  } catch (err) {
+    console.error(err)
+    return res.status(201).send()
+  }
 })
